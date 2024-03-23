7d770ae937fd4fabe24822143472959294b370f7
Thiago Sauer Govea <thiago.govea@caixa.gov.br>
2024-03-22 21:01:34 -0300

    Desabilita docs

--- app/page.js
+++ app/page.js
@@ -62,7 +62,8 @@ export default function Home() {
                 <a
                     href="#"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    rel="noopener noreferrer"
+                    // rel="noopener noreferrer"
+                    style={{ cursor: "not-allowed" }}
                 >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
                         Documentação{" "}
@@ -75,7 +76,7 @@ export default function Home() {
                     </p>
                 </a>
             </div>
-        </main>
+        </main >
     );
 }
 
